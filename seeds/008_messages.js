a85bb3ea9cc178f9dc6b277306237778165c1eb4
Christian Bourlier <chrisbourlier@hotmail.com>
2018-12-09 17:32:06 -0700

    messages seed

---
+++ seeds/008_messages.js
@@ -0,0 +1,25 @@
+
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('messages').del()
+    .then(() => Promise.all([
+      // Inserts seed entries
+      knex('messages').insert([
+        { id: 100801,
+          title: 'Remember to fill out your TPS reports',
+          body: 'If you could fill out that TPS report as soon as possible, that would be, ummmmmmmmm, greaaaaat.',
+          user: 100103
+        },
+        { id: 100802,
+          title: 'Saturday and Sunday Work',
+          body: `I'm going to need all of you to come into work on Saturday... Ummmmmmmmm yeaaaaaahhhhhh, aaaaand also on Sunday. Mmmkay thanks. *Takes sip of coffee*`,
+          user: 100102
+        },
+        { id: 100803,
+          title: 'The Subtle Art of Paper Jamming',
+          body: 'My name is Michael Bolton and I hate copy machines.',
+          user: 100101
+        },
+      ])
+    ]))
+}
